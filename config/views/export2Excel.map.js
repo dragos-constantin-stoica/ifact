016acac140a530b8caa044d5e3898952fff1d20f
dragos-constantin-stoica <dragos_constantin_stoica@yahoo.com>
2017-10-04 15:59:35 +0200

    Close #1 - Excel export of invoices and payments.

--- config/views/export2Excel.map.js
+++ config/views/export2Excel.map.js
@@ -1,5 +1,26 @@
 function(doc){
-    if (typeof doc.doctype !== 'undefined' && doc.doctype == "INVOICE") {
-        emit(null,doc);
+    if (typeof doc.doctype !== 'undefined'){
+        if (doc.doctype == "INVOICE") {
+           emit(doc.doctype, {
+               id: doc._id,
+               numar: doc.SERIA + " " + doc.NUMARUL,
+               INVOICE_DATE: doc.INVOICE_DATE,
+               DUE_DATE: doc.DUE_DATE,
+               detalii: doc.BENEFICIAR.nume,
+               suma_eur: (doc.FURNIZOR.valuta == "EUR")?doc.INVOICE_TOTAL:(doc.INVOICE_TOTAL/doc.CURS_BNR.eur_ron),
+               suma_ron: (doc.FURNIZOR.valuta == "RON")?doc.INVOICE_TOTAL:(doc.INVOICE_TOTAL*doc.CURS_BNR.eur_ron),
+               eur_ron: doc.CURS_BNR.eur_ron,
+               valuta: doc.FURNIZOR.valuta,
+               plata: []
+            });
+        }
+
+        if (doc.doctype == "PAYMENT"){
+            emit(doc.doctype, {
+                id: doc.invoice_id,
+                PAYMENT_DATE: doc.PAYMENT_DATE,
+                PAYMENT_SUM: doc.PAYMENT_SUM
+            });
+        }
     }
 }
--- globallists/lists/toxls.js
+++ globallists/lists/toxls.js
@@ -2,17 +2,98 @@ function (head, req) {
     // specify that we're providing a JSON response
     provides('json', function() {
         // create an array for our result set
-        var results = [];
+        var results = [], invoices = {};
+
 
         while (row = getRow()) {
-            results.push([
-                row.value.SERIA + " " + row.value.NUMARUL,
-                row.value.INVOICE_DATE,
-                row.value.DUE_DATE,
-                row.value.BENEFICIAR.nume,
-                row.value.INVOICE_TOTAL
-            ]);
+
+            if (row.key == "INVOICE"){
+                if (typeof invoices[row.value.id] === 'undefined'){
+                    invoices[row.value.id]= row.value;
+                    invoices[row.value.id].plata = [];
+                }else{
+                    var plata = invoices[row.value.id].plata;
+                    invoices[row.value.id] = row.value;
+                    invoices[row.value.id].plata = plata;
+                }
+            }
+
+            if (row.key == "PAYMENT"){
+                if (typeof invoices[row.value.id] === 'undefined'){
+                    invoices[row.value.id] = {};
+                    invoices[row.value.id].plata = [];
+                    invoices[row.value.id].plata.push(row.value);
+                }else{
+                    invoices[row.value.id].plata.push(row.value);
+                    invoices[row.value.id].plata.sort(function(a, b) {
+                        var aDATE = new Date(a.PAYMENT_DATE.substr(-4),a.PAYMENT_DATE.substr(3,2), a.PAYMENT_DATE.substr(0,2)), 
+                            bDATE = new Date(b.PAYMENT_DATE.substr(-4),b.PAYMENT_DATE.substr(3,2), b.PAYMENT_DATE.substr(0,2));
+                        return aDATE - bDATE;
+                      });
+                }
+            }
         }
+
+        var tmpResult = [];
+        for (var key in invoices) {
+            tmpResult.push(invoices[key]);
+        }
+
+        tmpResult.sort(function(a,b){
+            return (a.id>b.id)?1:(a.id<b.id?-1:0);
+        });
+
+        results.push([
+            "Achitată" , "Numă factură" , "Data facturii" , "Data scadenţă" , "Detalii factură" , "Suma €UR" , "Suma RON" , 
+            "Virament €UR"  , "Virament RON" , "Data virament"
+        ]);
+
+        tmpResult.forEach(function(elm){
+            if (elm.plata.length == 0){
+                results.push([
+                    "",
+                    elm.numar,
+                    elm.INVOICE_DATE,
+                    elm.DUE_DATE,
+                    elm.detalii,
+                    elm.suma_eur,
+                    elm.suma_ron,
+                    "","",""
+                ]);
+            }else{
+                for(var i=0; i< elm.plata.length; i++){
+                    var pay_eur = (elm.valuta == "EUR")?elm.plata[i].PAYMENT_SUM:(elm.plata[i].PAYMENT_SUM/elm.eur_ron), 
+                    pay_ron = (elm.valuta == "RON")?elm.plata[i].PAYMENT_SUM:(elm.plata[i].PAYMENT_SUM*elm.eur_ron);
+                    if (i==0){
+                        results.push([
+                            "",
+                            elm.numar,
+                            elm.INVOICE_DATE,
+                            elm.DUE_DATE,
+                            elm.detalii,
+                            elm.suma_eur,
+                            elm.suma_ron,
+                            pay_eur,
+                            pay_ron,
+                            elm.plata[i].PAYMENT_DATE
+                        ]);
+                    }else{
+                        results.push([
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            "",
+                            pay_eur,
+                            pay_ron,
+                            elm.plata[i].PAYMENT_DATE
+                        ]);
+                    }
+                }
+            }
+        });
 
         // make sure to stringify the results :)
         send(JSON.stringify(results));
